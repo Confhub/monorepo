808c7fb6a965bb4cab9b5eb13948ddf059f02df7
Yuri Yakovlev <hi@mynameisyuri.com>
2018-08-29 02:30:47 +0200

    Add roles on frontend

--- components/layout/Header/Header.js
+++ components/layout/Header/Header.js
@@ -40,36 +40,48 @@ const Header = ({ isAuth, userData, signOut }) => (
         </Menu.Item>
       )}
 
+      {isAuth &&
+        userData.role === "MODERATOR" && (
+          <Menu.Item key="admin">
+            <Link href="/admin" prefetch>
+              <a>
+                <Icon type="dashboard" />
+                Admin Panel
+              </a>
+            </Link>
+          </Menu.Item>
+        )}
+
       {isAuth && (
-        <Menu.Item key="admin">
-          <Link href="/admin" prefetch>
-            <a>
-              <Icon type="dashboard" />
-              Admin Panel
+        <Menu.SubMenu
+          title={
+            <span>
+              <Icon type="user" />
+              <span>{`${userData.name} (${userData.email})`}</span>
+            </span>
+          }
+        >
+          <Menu.Item key="profile">
+            <Icon type="profile" /> Profile
+          </Menu.Item>
+          <Menu.Item key="settings">
+            <Icon type="setting" /> Settings
+          </Menu.Item>
+
+          <Menu.Item key="signout">
+            <a onClick={signOut}>
+              <Icon type="poweroff" />
+              Sign Out
             </a>
-          </Link>
-        </Menu.Item>
+          </Menu.Item>
+        </Menu.SubMenu>
       )}
 
       {isAuth && (
-        <Menu.Item key="signout">
-          <a onClick={signOut}>
-            <Icon type="poweroff" />
-            Sign Out{" "}
-            {userData ? (
-              <span>
-                &#40;
-                {userData.email}
-                &#41;
-              </span>
-            ) : null}
-          </a>
-        </Menu.Item>
+        <div className={`role-badge ${userData.role.toLowerCase()}`}>
+          {userData.role}
+        </div>
       )}
-
-      {/* <Menu.Item key="user">
-        <p>Hello</p>
-        </Menu.Item> */}
     </Menu>
 
     <style jsx>{`
@@ -80,6 +92,31 @@ const Header = ({ isAuth, userData, signOut }) => (
         margin: 16px 24px 16px 0;
         float: left;
         cursor: pointer;
+      }
+
+      .role-badge {
+        display: inline-block;
+        height: 20px;
+        padding: 0 8px;
+        border-radius: 10px;
+        min-width: 20px;
+        background: #d9d9d9;
+        color: #fff;
+        line-height: 20px;
+        font-size: 12px;
+        box-shadow: 0 0 0 1px #fff;
+
+        &.attendee {
+          background: #52c41a;
+        }
+
+        &.speaker {
+          background: #1890ff;
+        }
+
+        &.moderator {
+          background: #f5222d;
+        }
       }
     `}</style>
   </Layout.Header>
